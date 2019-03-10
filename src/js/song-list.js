9a5ca52f2abbe0f5cbdcd242bdcbb881b8f99fad
Dylan <huanglinjay123@outlook.com>
2019-03-10 18:06:46 +0800

    点击列表中的歌曲，该歌曲addClass高亮，其他歌曲不高亮，新建歌曲也不高亮。

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,11 +9,15 @@
             let $el=$(this.el)
             $el.html(this.template)
             let {songs}=data
-            let liList=songs.map((song)=>$('<li></li>').text(song.name))
+            let liList=songs.map((song)=>$('<li></li>').text(song.name).attr('data-song-id',data.id))
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li){
+            let $li=$(li)
+            $li.addClass('active').siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -38,6 +42,11 @@
             this.view=view
             this.model=model
             this.view.render(this.model.data)
+            this.bindEventHub()
+            this.getAllSongs()
+            this.bindEvents()          
+        },
+        bindEventHub(){
             window.eventHub.on('upload',()=>{
                 this.view.clearActive()
             })
@@ -45,7 +54,16 @@
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
-            this.model.find().then(()=>{
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId=e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})    
+            })   
+        },
+        getAllSongs(){
+            return this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
