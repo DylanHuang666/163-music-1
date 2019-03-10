a77af565edf20d69c592b709e2ccf9c9ad8a18bf
Dylan <huanglinjay123@outlook.com>
2019-03-10 16:00:48 +0800

    MVC

---
+++ src/js/initializers/av.js
@@ -0,0 +1,9 @@
+{
+    let APP_ID = 'uopniNrop0QlmICi8TzlqKYO-9Nh9j0Va';
+    let APP_KEY = 'KEIIuo5oBbI8Q9eeSeio5Q43';
+
+    AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+    });
+}
---
+++ src/js/song-form.js
@@ -0,0 +1,105 @@
+
+
+{
+    let view={
+        el:'.page>main',
+        init(){
+            this.$el=$(this.el)
+        },
+        template:`
+        <h1>新建歌曲</h1>
+        <form class="form">
+            <div class="row">
+                <label for="">
+                    歌名                       
+                </label>
+                <input type="text" name="name" value="__name__">
+            </div>
+            <div class="row">
+                <label for="">
+                    歌手    
+                </label>
+                <input type="text" name="singer" value="__singer__">
+            </div>
+            <div class="row">
+                <label for="">
+                    外链                      
+                </label>
+                <input type="text" name="url" value="__url__">
+            </div>
+            <div class="row actions">
+                <button type="submit">保存</button>
+            </div>
+        </form>
+        `,
+        render(data={}){
+            let placeholders=['name','singer','url','id']
+            let html=this.template
+            placeholders.map((string)=>{
+                html=html.replace(`__${string}__`,data[string]||'')
+            })
+            $(this.el).html(html)
+        },
+        reset(){
+            this.render({})
+        }
+    }
+    let model={
+        data:{
+            name:'',singer:'',url:'',id:''
+        },
+        create(data){ //保存数据到数据库
+            // 声明类型
+            var Song = AV.Object.extend('Song');
+            // 新建对象
+            var song = new Song();
+            // 设置名称
+            song.set('name',data.name);
+            song.set('singer',data.singer);
+            song.set('url',data.url);
+            // // 设置优先级
+            // songr.set('priority',1);
+            return song.save().then((newSong)=> {
+                let {id,attributes}=newSong
+                Object.assign(this.data,{ //model.data
+                    id:id,
+                    name:attributes.name,    //这四句话相等于{id,...attributes}
+                    singer:attributes.singer,
+                    url:attributes.url
+                })
+            }, function (error) {
+                console.error(error);
+            });
+        }
+    }
+    let controller={
+        init(view,model){
+            this.view=view
+            this.view.init()
+            this.model=model  
+            this.view.render(this.model.data)
+            this.bindEvents()
+            window.eventHub.on('upload',(data)=>{
+                this.model.data=data
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            this.view.$el.on('submit','form',(e)=>{
+                e.preventDefault()
+                let needs='name singer url'.split(' ')
+                let data={}
+                needs.map((string)=>{
+                    data[string]=this.view.$el.find(`[name="${string}"]`).val() //得到data(nama,singer,url)
+                })
+                this.model.create(data).then(()=>{
+                    this.view.reset()
+                    let string=JSON.stringify(this.model.data)
+                    let object=JSON.parse(string)
+                    window.eventHub.emit('create',object)                 
+                })
+            })
+        }
+    }
+    controller.init(view,model)
+}
