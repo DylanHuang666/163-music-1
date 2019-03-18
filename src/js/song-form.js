aac3753bf99686a5b1d3d516ce377c384e82f112
Dylan <huanglinjay123@outlook.com>
2019-03-18 15:01:19 +0800

    加了update事件，更新歌曲信息，song-list也相应更新

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -74,6 +74,16 @@
             }, function (error) {
                 console.error(error);
             });
+        },
+        updata(data){
+            var song = AV.Object.createWithoutData('Song', this.data.id);
+            song.set('name', data.name);
+            song.set('url', data.url);
+            song.set('singer', data.singer);  
+            return song.save().then((response)=>{
+                Object.assign(this.data,data)
+                return response
+            })
         }
     }
     let controller={
@@ -102,20 +112,37 @@
                 this.view.render(this.model.data)
             })
         },
+        create(){
+            let needs='name singer url'.split(' ')
+            let data={}
+            needs.map((string)=>{
+                data[string]=this.view.$el.find(`[name="${string}"]`).val() //得到data(nama,singer,url)
+            })
+            this.model.create(data).then(()=>{
+                this.view.reset()
+                let string=JSON.stringify(this.model.data)
+                let object=JSON.parse(string)
+                window.eventHub.emit('create',object)                 
+            })
+        },
+        updata(){
+            let needs='name singer url'.split(' ')
+            let data={}
+            needs.map((string)=>{
+                data[string]=this.view.$el.find(`[name="${string}"]`).val() //得到data(nama,singer,url)
+            })
+            this.model.updata(data).then(()=>{
+                window.eventHub.emit('update',JSON.parse(JSON.stringify(this.model.data)))
+            })
+        },
         bindEvents(){
             this.view.$el.on('submit','form',(e)=>{
                 e.preventDefault()
-                let needs='name singer url'.split(' ')
-                let data={}
-                needs.map((string)=>{
-                    data[string]=this.view.$el.find(`[name="${string}"]`).val() //得到data(nama,singer,url)
-                })
-                this.model.create(data).then(()=>{
-                    this.view.reset()
-                    let string=JSON.stringify(this.model.data)
-                    let object=JSON.parse(string)
-                    window.eventHub.emit('create',object)                 
-                })
+                if(this.model.data.id){
+                    this.updata()
+                }else{
+                    this.create()
+                }
             })
         }
     }
--- src/js/song-list.js
+++ src/js/song-list.js
@@ -57,6 +57,15 @@
             window.eventHub.on('new',()=>{
                 this.view.clearActive()
             })
+            window.eventHub.on('update',(song)=>{
+                let songs=this.model.data.songs
+                for(let i=0;i<songs.length;i++){
+                    if(songs[i].id===song.id){
+                        Object.assign(songs[i],song)
+                    }
+                }
+                this.view.render(this.model.data)
+            })
         },
         bindEvents(){
             $(this.view.el).on('click','li',(e)=>{
