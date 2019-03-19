399e5afa76f9be54f8e647c00a64d5826bd16262
Dylan <huanglinjay123@outlook.com>
2019-03-19 12:53:31 +0800

    更新歌曲信息后，song-list又render了一遍，而激活态依然保持

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,8 +8,14 @@
         render(data){
             let $el=$(this.el)
             $el.html(this.template)
-            let {songs}=data
-            let liList=songs.map((song)=>$('<li></li>').text(song.name).attr('data-song-id',song.id))
+            let {songs,selectSongId}=data
+            let liList=songs.map((song)=>{
+                let $li=$('<li></li>').text(song.name).attr('data-song-id',song.id)
+                if(song.id===selectSongId){
+                    $li.addClass('active')
+                }
+                return $li
+            })
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
@@ -25,7 +31,8 @@
     }
     let model={
         data:{
-            songs:[]
+            songs:[],
+            selectSongId:undefined
         },
         find(){
             var query = new AV.Query('Song');
@@ -47,9 +54,9 @@
             this.bindEvents()          
         },
         bindEventHub(){
-            window.eventHub.on('upload',()=>{
-                this.view.clearActive()
-            })
+            // window.eventHub.on('upload',()=>{
+            //     this.view.clearActive()
+            // })
             window.eventHub.on('create',(songData)=>{
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
@@ -57,7 +64,7 @@
             window.eventHub.on('new',()=>{
                 this.view.clearActive()
             })
-            window.eventHub.on('update',(song)=>{
+            window.eventHub.on('update',(song)=>{//更新歌曲后更改this.model.data，再render一次
                 let songs=this.model.data.songs
                 for(let i=0;i<songs.length;i++){
                     if(songs[i].id===song.id){
@@ -69,8 +76,10 @@
         },
         bindEvents(){
             $(this.view.el).on('click','li',(e)=>{
-                this.view.activeItem(e.currentTarget)//高亮点击的歌曲
+                // this.view.activeItem(e.currentTarget)//高亮点击的歌曲
                 let songId=e.currentTarget.getAttribute('data-song-id')
+                this.model.data.selectSongId=songId
+                this.view.render(this.model.data)
                 let data
                 let songs=this.model.data.songs
                 for(let i = 0; i<songs.length; i++){
